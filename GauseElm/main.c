a7f7bea9091046b62aea6df2d9851b9a4cc7a8d6
Enchan <enchant0130@gmail.com>
2020-01-06 10:44:31 +0900

    Initial Commit

---
+++ GauseElm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GauseElm
+//
+//  Created by EnchantCode on 2020/01/06.
+//  Copyright © 2020 EnchantCode. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
